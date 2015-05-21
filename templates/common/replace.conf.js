c9d9a3cc456b22c0c1c2fa7c3e8a579f40095a88
Shahar Talmi <shahar.talmi@gmail.com>
2015-05-21 12:08:52 +0300

    preparing for process-vm

--- templates/common/replace.conf.js
+++ templates/common/replace.conf.js
@@ -1,9 +1,13 @@
 'use strict';
 
+var experiments = {
+
+};
+
 module.exports = {
   '${debug}': 'true',
   '${enableMocks}': 'false',
-  '${experiments}': '{}',
+  '${experiments}': JSON.stringify(experiments),
   '${locale}': 'en',<% if (dashboardApp) { %>
   '${metaSiteId}': 'c853c829-503e-48b1-892f-28d8c22a887c',<% } %>
 
