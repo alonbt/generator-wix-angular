9daa974acd5b62c92eadec17419d9d70c302252d
Shahar Talmi <shahart@wix.com>
2015-07-15 09:52:15 +0300

    made constant/value more typescript

--- templates/typescript/service/constant.ts
+++ templates/typescript/service/constant.ts
@@ -1,6 +1,11 @@
 /// <reference path="../../../reference.ts" />
 'use strict';
 
+class <%= classedName %> {
+  theQuestion: string = 'The Ultimate Question of Life, the Universe, and Everything';
+  theAnswer: number = 42;
+}
+
 angular
   .module('<%= scriptAppName %>Internal')
-  .constant('<%= cameledName %>', 42);
+  .constant('<%= classedName %>', new <%= classedName %>());
--- templates/typescript/service/value.ts
+++ templates/typescript/service/value.ts
@@ -1,6 +1,11 @@
 /// <reference path="../../../reference.ts" />
 'use strict';
 
+class <%= classedName %> {
+  theQuestion: string = 'The Ultimate Question of Life, the Universe, and Everything';
+  theAnswer: number = 42;
+}
+
 angular
   .module('<%= scriptAppName %>Internal')
-  .value('<%= cameledName %>', 42);
+  .value('<%= classedName %>', new <%= classedName %>());
