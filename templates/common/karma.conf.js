6dd13fd74c008345928d1a0a5e6133a6455e6d89
Shahar Talmi <shahart@wix.com>
2015-11-23 22:10:16 +0200

    fix karma

--- templates/common/karma.conf.js
+++ templates/common/karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function (config) {
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-translate/angular-translate.js',
+      'app/bower_components/uri.js/src/URI.js',
       'app/bower_components/wix-angular/dist/wix-angular.js',
       'app/bower_components/es5-shim/es5-shim.js',
       'app/bower_components/wix-bi-logger/dist/scripts/bi-logger/wix-bi-angular.js',<% if (tpa) { %>
